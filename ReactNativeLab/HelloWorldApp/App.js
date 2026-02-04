06c55e496972cd287ce1847679a36ed01ad20a7b
DoniZXCroni <136065842+DoniZefironi@users.noreply.github.com>
2026-02-04 13:51:41 +0300

    feat: add lab3

--- ReactNativeLab/HelloWorldApp/App.js
+++ ReactNativeLab/HelloWorldApp/App.js
@@ -12,16 +12,15 @@ export default function App() {
       colors={['#ffffff', '#07a0ff']} 
       style={styles.container}
     >
-      <Text style={styles.title}>Журнал Bright</Text>
+      <Text style={styles.title}>5 книжных новинок октября</Text>
       <View style={styles.containerInContainer}>
         <Text style={styles.link} onPress={openLink}>
-          Новости
+          Читать далее
         </Text>
-        <Image 
-          source={{ uri: 'https://brightmagazine.ru/wp-content/uploads/2025/12/asian-woman-wearing-japanese-traditional-kimono-fuji-mountain-sunset-kawaguchiko-lake-japan-600x400.jpg' }} 
-          style={styles.image}
-        />
-        <Text style={styles.subtitle}>Превращаем стресс в своего помощника</Text>
+        <Text style={styles.subtitle}>"Кадиш.com" Натан Ингландер.</Text>
+        <Text style={styles.subtitle}>Издательство "Книжники"</Text>
+      </View>
+      <View style={styles.containerInContainerBottom}>
         <Text style={styles.text}>
           Исследователи Йельского университета заявляют...
         </Text>
@@ -34,28 +33,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 100,
+    paddingTop: 50,
     alignItems: 'center', 
   },
   title: {
-    fontSize: 32,
+    fontSize: 24,
     fontWeight: '600',
     color: '#004749',
-    textAlign: 'center',
     marginBottom: 20,
   },
   containerInContainer: {
-    flex: 1,
     backgroundColor: '#FFFFFF',
-    width: '90%', 
-    padding: 24,
+    width: '100%', 
+    paddingTop: 24,
+    paddingLeft: 24,
+    paddingRight: 24,
     marginTop: 20,
-    borderRadius: 8,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3, 
+    alignItems: 'center',
+  },
+  containerInContainerBottom: {
+    flex: 1,
+    backgroundColor: '#afafaf',
+    width: '100%', 
+    alignItems: 'center',
+    padding: 24,
   },
   link: {
     color: '#007AFF',
@@ -63,13 +64,6 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     marginBottom: 16,
     textDecorationLine: 'underline',
-  },
-  image: {
-    width: '100%',
-    height: 200,
-    borderRadius: 8,
-    marginBottom: 16,
-    backgroundColor: '#f0f0f0', 
   },
   subtitle: {
     fontSize: 22,
